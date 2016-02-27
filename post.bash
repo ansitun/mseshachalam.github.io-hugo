771a51b09b0b3b391deeed509f7efa088cae60e7
Seshachalam Malisetti <seshachalam@outlook.com>
2016-02-27 23:52:44 +0530

    bash corrections

--- post.bash
+++ post.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ PB_BODY ]
+
 then
-curl --header 'Access-Token: $PB_TOKEN' \
+
+curl --header "Access-Token: $PB_TOKEN" \
        --header 'Content-Type: application/json' \
-       --data-binary $PB_BODY \
+       --data-binary "$PB_BODY" \
        --request POST \
        https://api.pushbullet.com/v2/pushes
 
