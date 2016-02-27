9ff4795be53767094764c89d39a52703abfd4287
Seshachalam Malisetti <seshachalam@outlook.com>
2016-02-28 00:00:51 +0530

    testing

--- process.bash
+++ process.bash
@@ -7,8 +7,7 @@ changed_files=$(git diff-tree --no-commit-id --name-only -r bb49a42)
 while IFS=' ' read -ra ADDR; do
   	for str in "${ADDR[@]}"; do
   		if [[ $str == "$pb_config"* ]]; then
-	  		echo "$str"
-	  		export PB_BODY=$(printf '{"type":"link","title":"%s","body":"%s","url":"%s","channel_tag":"indiadailynews"}\n' "$title" "$body" "$url")
+	  		export PB_BODY=$(<$str)
 	  		break
 		fi
 	done
