9d5249be5c049f6d72a39c142bc6768ab85e9051
Seshachalam Malisetti <seshachalam@outlook.com>
2016-02-27 23:35:31 +0530

    boo

--- post.bash
+++ post.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+if [ PB_BODY ]
+then
 curl --header 'Access-Token: $PB_TOKEN' \
        --header 'Content-Type: application/json' \
        --data-binary $PB_BODY \
        --request POST \
        https://api.pushbullet.com/v2/pushes
+
+fi
